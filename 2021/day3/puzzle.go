c9aea34db5b53c82bb496737c76cf4fd62a7efe4
Oscar Hellström <oscar@hellstrom.st>
2021-12-10 22:34:29 +0100

    Day 3 part 2

--- 2021/day3/puzzle.go
+++ 2021/day3/puzzle.go
@@ -32,6 +32,61 @@ func part1() int {
 	return gamma * epsilon
 }
 
+func part2() int {
+	oxygen := util.LoadBinaryInts()
+	co2 := util.LoadBinaryInts()
+
+	for x := bits -1; x > -1; x -= 1 {
+		bitcount := 0
+		for _, value := range oxygen {
+			bitcount += value >> x & 0x1
+		}
+
+		criteria := 0
+		if bitcount * 2 >= len(oxygen) {
+			criteria = 1
+		}
+
+
+		if len(oxygen) > 1 {
+			n := 0
+			for _, value := range oxygen {
+				if value >> x & 0x1 == criteria {
+					oxygen[n] = value
+					n += 1
+				}
+
+			}
+			oxygen = oxygen[:n]
+		}
+
+		bitcount = 0
+		for _, value := range co2 {
+			bitcount += value >> x & 0x1
+		}
+
+		criteria = 0
+		if bitcount * 2 < len(co2) {
+			criteria = 1
+		}
+
+		if len(co2) > 1 {
+			n := 0
+			for _, value := range co2 {
+				if value >> x & 0x1 == criteria {
+					co2[n] = value
+					n += 1
+				}
+
+			}
+			co2 = co2[:n]
+		}
+	}
+
+	return oxygen[0] * co2[0]
+}
+
 func main() {
 	fmt.Printf("Part 1: %d\n", part1())
+	fmt.Printf("Part 2: %d\n", part2())
 }
