a82a2b7523f11e9ce417adc3ba436b8bea2c633e
Oscar Hellström <oscar.hellstrom@vinnter.se>
2024-12-14 23:05:14 +0100

    Day 2 complete

--- 2024/src/day02.rs
+++ 2024/src/day02.rs
@@ -1,29 +1,72 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
-fn is_safe(report: &str) -> bool {
-    let mut levels = report.split_whitespace();
-    let mut prev = levels.next().unwrap().parse::<i32>().unwrap();
-    let mut was_increasing = None;
-    for current in levels.map(|level| level.parse::<i32>().unwrap()) {
-        let difference = current - prev;
+fn level_is_safe(prev_level: i32, level: i32, was_increasing: Option<bool>) -> bool {
+    let diff = level - prev_level;
 
-        let is_increasing = current > prev;
-        if let Some(was_increasing) = was_increasing {
-            if was_increasing != is_increasing {
-                return false;
-            }
-        }
-        was_increasing = Some(is_increasing);
-        let abs_difference = i32::abs(difference);
-        if !(1..=3).contains(&abs_difference) {
+    if !(1..=3).contains(&i32::abs(diff)) {
+        return false;
+    }
+
+    let is_increasing = level > prev_level;
+
+    if let Some(was_increasing) = was_increasing {
+        if is_increasing != was_increasing {
             return false;
         }
-
-        prev = current;
     }
 
     true
+}
+
+fn report_is_safe(levels: &[i32], dampener: bool) -> bool {
+    let mut prev = levels[0];
+    let mut increasing = None;
+    for (count, level) in levels[1..].iter().enumerate() {
+        let idx = count + 1;
+        if !level_is_safe(prev, *level, increasing) {
+            if dampener {
+                // try to remove the value before the previous one
+                // this can caused the "direction" to become false
+                if idx >= 2 {
+                    let mut modified_report = Vec::new();
+                    modified_report.extend_from_slice(&levels[0..idx - 2]);
+                    modified_report.extend_from_slice(&levels[idx - 1..]);
+                    if report_is_safe(&modified_report, false) {
+                        return true;
+                    }
+                }
+
+                // try remove the previous value and test this
+                let mut modified_report = Vec::new();
+                modified_report.extend_from_slice(&levels[0..idx - 1]);
+                modified_report.extend_from_slice(&levels[idx..]);
+                if report_is_safe(&modified_report, false) {
+                    return true;
+                }
+
+                let mut modified_report = Vec::new();
+                modified_report.extend_from_slice(&levels[0..idx]);
+                modified_report.extend_from_slice(&levels[idx + 1..]);
+                if report_is_safe(&modified_report, false) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        increasing = Some(*level > prev);
+        prev = *level;
+    }
+
+    true
+}
+
+fn is_safe(report: &str, dampner: bool) -> bool {
+    let levels = report
+        .split_whitespace()
+        .map(|level| level.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+    report_is_safe(&levels[..], dampner)
 }
 
 fn part1() -> usize {
@@ -31,10 +74,20 @@ fn part1() -> usize {
     io::BufReader::new(input)
         .lines()
         .map(|report| report.unwrap())
-        .filter(|report| is_safe(report))
+        .filter(|report| is_safe(report, false))
+        .count()
+}
+
+fn part2() -> usize {
+    let input = File::open("input/input_02.txt").unwrap();
+    io::BufReader::new(input)
+        .lines()
+        .map(|report| report.unwrap())
+        .filter(|report| is_safe(report, true))
         .count()
 }
 
 pub fn run() {
     println!("Part1: {}", part1());
+    println!("Part2: {}", part2());
 }
